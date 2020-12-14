c5a8345cfd9513536f8fb5022139926347a070ce
Vincent.Brocheton <vinc.brocheton@gmail.com>
2020-12-14 10:06:17 +0100

    last modif

--- Exo3/script.js
+++ Exo3/script.js
@@ -11,44 +11,43 @@ let bisextile = false;
 let valide = false;
 
 form.addEventListener("submit", (e) => {
-  result.innerHTML = "";
-  e.preventDefault();
-  let jour = Number(inputJour.value);
-  let mois = Number(inputMois.value);
-  let annee = Number(inputAnnee.value);
+    e.preventDefault();
+    let jour = Number(inputJour.value);
+    let mois = Number(inputMois.value);
+    let annee = Number(inputAnnee.value);
 
-  isBisextile(annee);
+    isBisextile(annee);
 
-  if (jour > 0 && mois > 0) {
-    if (
-      jour < 32 &&
-      mois < 13 &&
-      moi30jours.includes(mois) === false &&
-      mois !== 2
-    ) {
-      valide = true;
-    } else if (jour < 31 && moi30jours.includes(mois) === true) {
-      valide = true;
-    }else if(jour<30 && mois ===2 && bisextile){
-        valide=true;
-    }else if(jour<29&& mois===2){
-        valide=true
-    }else {
-        valide=false
-    }  
-  }
+    if (jour > 0 && mois > 0) {
+        if (
+            jour < 32 &&
+            mois < 13 &&
+            moi30jours.includes(mois) === false &&
+            mois !== 2
+        ) {
+            valide = true;
+        } else if (jour < 31 && moi30jours.includes(mois) === true) {
+            valide = true;
+        } else if (jour < 30 && mois === 2 && bisextile) {
+            valide = true;
+        } else if (jour < 29 && mois === 2 && !bisextile) {
+            valide = true
+        } else {
+            valide = false
+        }
+    }
 
-  if (valide) {
-    result.innerText = "La date est valide";
-  } else {
-    result.innerText = "La date est invalide";
-  }
+    if (valide) {
+        result.innerText = "La date est valide";
+    } else {
+        result.innerText = "La date est invalide";
+    }
 });
 
-function isBisextile(annee){
-    if((annee%4 ===0 &&annee%100!==0) ||annee%400===0){
-        bisextile=true;
-    }else {
-        bisextile=false;
+function isBisextile(annee) {
+    if ((annee % 4 === 0 && annee % 100 !== 0) || annee % 400 === 0) {
+        bisextile = true;
+    } else {
+        bisextile = false;
     }
 }
